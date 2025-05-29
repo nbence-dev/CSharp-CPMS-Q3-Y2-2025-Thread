9f0f2d6216e016fd0985da3125f201bbb9327853
Nicholas <nicholasbence12@gmail.com>
2025-05-29 17:22:27 +0200

    Complete version. Need revise

---
+++ Question3_New/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Question3_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================================================");
+            Console.WriteLine("                     Drone Racing");
+            
+            Console.WriteLine("                 Let the race begin!");
+            Console.WriteLine("==========================================================");
+            Thread.Sleep(1500);
+            List<drone> drones = new List<drone>();
+            // Create 5 drones
+            for (int i = 0; i < 5; i++)
+            {
+                drones.Add(new drone(i+1));
+            }
+            //A list of each thread (5 threads)
+            List<Thread> threads = new List<Thread>();
+            foreach (drone Drone in drones)
+            {
+                // Create a new thread for each drone and start it
+                Thread thread = new Thread(Drone.StartRace);
+                //Add the thread to the list of threads
+                threads.Add(thread);
+                thread.Start();
+            }
+            foreach (Thread thread in threads)
+            {
+                // Wait for each thread to finish
+                thread.Join();
+            }
+            drone winner = drone.GetWinner();
+            Console.WriteLine("==========================================================");
+            Console.WriteLine($"\nThe winner is Drone {winner.Number}!");
+            Console.WriteLine("All drones have completed the race!");  
+            //Ensure all drones complete the race before exiting the program
+
+        }
+        
+    }
+}
---
+++ Question3_New/drone.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3_New
+{
+    internal class drone
+    {
+        // Drone gets assigned a number
+        public int Number { get; }
+
+        // Only a drone can access its own distance travelled
+        public int DistanceTravelled { get; private set; } = 0;
+        // A static variable to keep track of the winner - a shared flag. I've learned that static variables are shared across all instances of a class.
+        private static drone winner = null;
+        // lock object - this ensures no thread can win at the same time, announcing inacurate results. This ensures for one winner only
+        private static object lockObj = new object();
+        public drone(int number)
+        {
+            Number = number;
+
+        }
+
+        public void StartRace()
+        {
+
+            Random rand = new Random();
+
+            while (DistanceTravelled < 25)
+            {
+
+                {
+                    DistanceTravelled += rand.Next(1, 6); // Randomly move between 1 and 5 km
+                    Console.WriteLine($"Drone {Number} has travelled {DistanceTravelled} km.");
+
+                    
+
+                    // Announcing the winner (locking the object)
+                    lock (lockObj)
+                    {
+                        if (winner == null && DistanceTravelled >= 25)
+                        {
+                            winner = this;
+                        }
+
+                    }
+                    Thread.Sleep(1500);
+
+
+                }
+            }
+            Console.WriteLine("==========================================================");
+            Console.WriteLine($"Drone {Number} finished the race!");
+            Console.WriteLine("==========================================================");
+        }
+        public static drone GetWinner()
+        {
+            // Return the winner
+            return winner;
+        }
+        
+
+
+    }
+}
+
